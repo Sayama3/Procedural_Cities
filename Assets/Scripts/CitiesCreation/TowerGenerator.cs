0c7dc05faeb7c6cfdf2761b5039d1f0c4c3821b9
Iannis Poquet <ianpo@live.fr>
2021-01-08 21:02:21 +0100

    Generate Tower with Quad

--- Assets/Scripts/CitiesCreation/TowerGenerator.cs
+++ Assets/Scripts/CitiesCreation/TowerGenerator.cs
@@ -1,129 +1,256 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace ProceduralCities.CitiesCreation
 {
-    [RequireComponent(typeof(MeshRenderer),typeof(MeshFilter))]
     public class TowerGenerator : MonoBehaviour
     {
-        private MeshRenderer _meshRenderer;
-        private MeshFilter _meshFilter;
-        //private BoxCollider box;
-        private Mesh mesh;
+        [SerializeField,ReadOnly]
         private Material _material;
         [SerializeField,ReadOnly]
         private Vector3 _size;
 
+        //private BoxCollider box;
+
+        [ShowInInspector, ReadOnly]
+        private float ChildCount
+        {
+            get
+            {
+                int totalCount = 0;
+                for (int i = 0; i < transform.childCount; i++)
+                    totalCount += transform.GetChild(i).childCount;
+                return totalCount;
+            }
+        }
+
         private static readonly int BaseMapSt = Shader.PropertyToID("_BaseMap_ST");
 
         //TODO: generate myself the mesh
+        [ShowIf("@ChildCount == 0")]
+        [Button(ButtonSizes.Medium,ButtonStyle.FoldoutButton)]
         public void Initialize(Vector3 size, Material material, float materialDivider = 1f)
         {
-            _meshRenderer = GetComponent<MeshRenderer>();
-            _meshFilter = GetComponent<MeshFilter>();
             _size = size;
-            //transform.localScale = new Vector3(size.x, height, size.y);
             _material = new Material(material);
-            _meshRenderer.material = _material;
-            mesh = new Mesh();
-            _meshFilter.mesh = mesh;
-            
             CreateCube(size,materialDivider);
-            //box = gameObject.AddComponent<BoxCollider>();
+            
+            // Creation of the collider if needed
+            // box = gameObject.AddComponent<BoxCollider>();
+            // box.center = new Vector3(0, size.y / 2f);
+            // box.size = size;
         }
         
         private void CreateCube (Vector3 size, float matDiv)
         {
+            //Drawing a cube this way
+            //    .h------g
+            //  .' |    .'|
+            // e---+--f'  |
+            // |   |  |   |
+            // |  ,d--+---c
+            // |.'  o | .' 
+            // a------b'    
+
+            Vector3 pos = transform.position;
+            
+            Vector3 a = pos + new Vector3(-size.x / 2f, 0, -size.z / 2f);
+            Vector3 b = pos + new Vector3(size.x / 2f, 0, -size.z / 2f);
+            Vector3 c = pos + new Vector3(size.x / 2f, 0, size.z / 2f);
+            Vector3 d = pos + new Vector3(-size.x / 2f, 0, size.z / 2f);
+            
+            Vector3 e = pos + new Vector3(-size.x / 2f, size.y, -size.z / 2f);
+            Vector3 f = pos + new Vector3(size.x / 2f, size.y, -size.z / 2f);
+            Vector3 g = pos + new Vector3(size.x / 2f, size.y, size.z / 2f);
+            Vector3 h = pos + new Vector3(-size.x / 2f, size.y, size.z / 2f);
+            
             float xmax = 2f * size.y + 2f * size.x;
             float ymax = 2f * size.y + size.z;
             
-            Vector3[] vertices = {
-                new Vector3(-size.x/2f, size.y, -size.z/2f),
-                new Vector3(-size.x/2f, 0, -size.z/2f),
-                new Vector3(size.x/2f, size.y, -size.z/2f),
-                new Vector3(size.x/2f, 0, -size.z/2f),
-
-                new Vector3(-size.x/2f, 0, size.z/2f),
-                new Vector3(size.x/2f, 0, size.z/2f),
-                new Vector3(-size.x/2f, size.y, size.z/2f),
-                new Vector3(size.x/2f, size.y, size.z/2f),
-
-                new Vector3(-size.x/2f, size.y, -size.z/2f),
-                new Vector3(size.x/2f, size.y, -size.z/2f),
-
-                new Vector3(-size.x/2f, size.y, -size.z/2f),
-                new Vector3(-size.x/2f, size.y, size.z/2f),
-
-                new Vector3(size.x/2f, size.y, -size.z/2f),
-                new Vector3(size.x/2f, size.y, size.z/2f),
-            };
-
-            int[] triangles = {
-                0, 2, 1, // front
-                1, 2, 3,
-                4, 5, 6, // back
-                5, 7, 6,
-                6, 7, 8, //top
-                7, 9 ,8, 
-                1, 3, 4, //bottom
-                3, 5, 4,
-                1, 11,10,// left
-                1, 4, 11,
-                3, 12, 5,//right
-                5, 12, 13
-            };
+            // Vector3[] vertices = {
+            //     new Vector3(-size.x/2f, size.y, -size.z/2f),
+            //     new Vector3(-size.x/2f, 0, -size.z/2f),
+            //     new Vector3(size.x/2f, size.y, -size.z/2f),
+            //     new Vector3(size.x/2f, 0, -size.z/2f),
+            //
+            //     new Vector3(-size.x/2f, 0, size.z/2f),
+            //     new Vector3(size.x/2f, 0, size.z/2f),
+            //     new Vector3(-size.x/2f, size.y, size.z/2f),
+            //     new Vector3(size.x/2f, size.y, size.z/2f),
+            //
+            //     new Vector3(-size.x/2f, size.y, -size.z/2f),
+            //     new Vector3(size.x/2f, size.y, -size.z/2f),
+            //
+            //     new Vector3(-size.x/2f, size.y, -size.z/2f),
+            //     new Vector3(-size.x/2f, size.y, size.z/2f),
+            //
+            //     new Vector3(size.x/2f, size.y, -size.z/2f),
+            //     new Vector3(size.x/2f, size.y, size.z/2f),
+            // };
+            //
+            // int[] triangles = {
+            //     0, 2, 1, // front
+            //     1, 2, 3,
+            //     4, 5, 6, // back
+            //     5, 7, 6,
+            //     6, 7, 8, //top
+            //     7, 9 ,8, 
+            //     1, 3, 4, //bottom
+            //     3, 5, 4,
+            //     1, 11,10,// left
+            //     1, 4, 11,
+            //     3, 12, 5,//right
+            //     5, 12, 13
+            // };
+            //
+            // Vector2[] uvs = {
+            //     new Vector2(0, (size.y+size.z)/ymax),
+            //     new Vector2((size.y)/xmax, (size.y+size.z)/ymax),
+            //     new Vector2(0, (size.y)/ymax),
+            //     new Vector2((size.y)/xmax, (size.y)/ymax),
+            //
+            //     new Vector2((size.y+size.x)/xmax, (size.y+size.z)/ymax),
+            //     new Vector2((size.y+size.x)/xmax, (size.y)/ymax),
+            //     new Vector2((size.y+size.x+size.y)/xmax, (size.y+size.z)/ymax),
+            //     new Vector2((size.y+size.x+size.y)/xmax, (size.y)/ymax),
+            //
+            //     new Vector2(1, (size.y+size.z)/ymax),
+            //     new Vector2(1, (size.y)/ymax),
+            //
+            //     new Vector2((size.y)/xmax, 1),
+            //     new Vector2((size.y+size.x)/xmax, 1),
+            //
+            //     new Vector2((size.y)/xmax, 0),
+            //     new Vector2((size.y+size.x)/xmax, 0),
+            // };
+            //
+            // mesh.Clear ();
+            // mesh.vertices = vertices;
+            // mesh.triangles = triangles;
+            // mesh.uv = uvs;
+            // mesh.Optimize ();
+            // mesh.RecalculateNormals ();
+            CreateFace(a, f, Face.Back);
+            CreateFace(d, g, Face.Forward);
             
-            Vector2[] uvs = {
-                new Vector2(0, (size.y+size.z)/ymax),
-                new Vector2((size.y)/xmax, (size.y+size.z)/ymax),
-                new Vector2(0, (size.y)/ymax),
-                new Vector2((size.y)/xmax, (size.y)/ymax),
-
-                new Vector2((size.y+size.x)/xmax, (size.y+size.z)/ymax),
-                new Vector2((size.y+size.x)/xmax, (size.y)/ymax),
-                new Vector2((size.y+size.x+size.y)/xmax, (size.y+size.z)/ymax),
-                new Vector2((size.y+size.x+size.y)/xmax, (size.y)/ymax),
-
-                new Vector2(1, (size.y+size.z)/ymax),
-                new Vector2(1, (size.y)/ymax),
-
-                new Vector2((size.y)/xmax, 1),
-                new Vector2((size.y+size.x)/xmax, 1),
-
-                new Vector2((size.y)/xmax, 0),
-                new Vector2((size.y+size.x)/xmax, 0),
-            };
-
-            mesh.Clear ();
-            mesh.vertices = vertices;
-            mesh.triangles = triangles;
-            mesh.uv = uvs;
-            mesh.Optimize ();
-            mesh.RecalculateNormals ();
+            CreateFace(a,h,Face.Left);
+            CreateFace(b,g,Face.Right);
+            
+            CreateFace(e,g,Face.Up);
+            CreateFace(a,c,Face.Down);
             
             _material.SetVector(BaseMapSt, new Vector4(xmax/matDiv, ymax/matDiv, 0, 0));
         }
 
-        // [Button(ButtonSizes.Medium)]
-        // public void CheckCollision()
-        // {
-        //     var colliders = Physics.OverlapBox(box.center, box.size/2f);
-        //     foreach (var col in colliders)
-        //     {
-        //         if (col.name.Equals("Quad") || col.name.Equals(name) || col == null) continue;
-        //         
-        //         Debug.Log(name + " collide with " + col.name);
-        //         //DestroyImmediate(col.gameObject);
-        //     }
-        //
-        //     DestroyCollider();
-        // }
-        //
-        // public void DestroyCollider()
-        // {
-        //     //Destroy(box);
-        // }
+        private void CreateFace(Vector3 a, Vector3 b, Face face)
+        {
+            #region Setup
+
+            Vector2 _a;
+            Vector2 _b;
+            float missingAxis;
+            Vector3 normal;
+            
+            if(face == Face.Up || face ==  Face.Down)
+            { 
+                //removing y
+                _a = new Vector2(a.x, a.z);
+                _b = new Vector2(b.x, b.z);
+                missingAxis = a.y;
+                normal = face == Face.Up ? Vector3.up : Vector3.down;
+            }
+            else if(face == Face.Right || face ==  Face.Left)
+            { 
+                //removing x
+                _a = new Vector2(a.z, a.y);
+                _b = new Vector2(b.z, b.y);
+                missingAxis = a.x;
+                normal = face == Face.Right ? Vector3.right : Vector3.left;
+            }
+            else if(face == Face.Forward || face ==  Face.Back)
+            {
+                //removing z
+                _a = new Vector2(a.x, a.y);
+                _b = new Vector2(b.x, b.y);
+                missingAxis = a.z;
+                normal = face == Face.Forward ? Vector3.forward : Vector3.back;
+            }
+            else
+                throw new ArgumentOutOfRangeException(nameof(face), face, null);
+
+            #endregion
+
+            GameObject quadTemp = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            DestroyImmediate(quadTemp.GetComponent<Collider>(),false);
+            Transform parent;
+            (parent = new GameObject(face.ToString()).transform).SetParent(transform);
+
+            for (float y = _a.y; y < _b.y; y++)
+            {
+                for (float x = _a.x; x < _b.x; x++)
+                {
+                    Vector3 position;
+                    Quaternion rotation;
+                    var realX = x + .5f;
+                    var realY = y + .5f;
+                    if (face == Face.Up || face == Face.Down)
+                    {
+                        position = new Vector3(realX, missingAxis, realY);
+                        rotation = Quaternion.LookRotation(-normal, face == Face.Up ? Vector3.forward : Vector3.back);
+                    }
+                    else if (face == Face.Right || face == Face.Left)
+                    {
+                        position = new Vector3(missingAxis, realY, realX);
+                        rotation = Quaternion.LookRotation(-normal, Vector3.up);
+                    }
+                    else if (face == Face.Forward || face == Face.Back)
+                    {
+                        position = new Vector3(realX, realY, missingAxis);
+                        rotation = Quaternion.LookRotation(-normal, Vector3.up);
+                    }
+                    else
+                        throw new ArgumentOutOfRangeException(nameof(face), face, null);
+
+                    var quad = Instantiate(quadTemp,position,rotation,parent);
+                    quad.name = "Quad - " + face + " - ( " + x.ToString("F1") +" : " + y.ToString("F1") +" )";
+                    
+                    //TODO: set the material depending on the procedural generation
+                    Material tempMat = new Material(_material);
+                    quad.GetComponent<MeshRenderer>().material = tempMat;
+                    tempMat.color = Random.ColorHSV();
+                }
+            }
+
+            DestroyImmediate(quadTemp, false);
+        }
+
+        [Button(ButtonSizes.Medium, ButtonStyle.FoldoutButton)]
+        private void HardReset()
+        {
+            for (int i = transform.childCount - 1; i >= 0; i--)
+            {
+                DestroyImmediate(transform.GetChild(i).gameObject);
+            }
+            _size = Vector3.zero;
+            _material = null;
+
+            if (TryGetComponent(out Collider col))
+                DestroyImmediate(col);
+        }
+
+        public enum Face
+        {
+            Up,
+            Forward,
+            Right,
+            Down,
+            Back,
+            Left,
+        }
     }
 }
---
+++ Assets/Scripts/Tester/QuadFiller.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Internal;
+
+namespace ProceduralCities.Tester
+{
+    public class QuadFiller : MonoBehaviour
+    {
+        [SerializeField] private Point quadPoints = new Point(z:0);
+        [SerializeField] private Color _colorLine = Color.green;
+        [SerializeField] private Color _colorSphere = Color.red;
+        [SerializeField,ReadOnly] private List<GameObject> quads;
+        [SerializeField, MinValue(0),MaxValue(1),ReadOnly] private Vector2 offset = new Vector2(.5f, .5f);
+
+        [Button(ButtonSizes.Medium)]
+        public void DrawQuads()
+        {
+            if (quads != null)
+            {
+                if(quads.Count >0)
+                {
+                    for (int i = quads.Count - 1; i >= 0; i--)
+                    {
+                        Destroy(quads[i]);
+                    }
+                }
+                quads.Clear();
+            }
+            quads = new List<GameObject>();
+            var refObj = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            DestroyImmediate(refObj.GetComponent<Collider>(),false);
+            
+            for (int y = quadPoints.LeftDown.y; y < quadPoints.RightUpper.y; y++)
+            {
+                for (int x = quadPoints.LeftDown.x; x < quadPoints.RightUpper.x; x++)
+                {
+                    Transform trans= transform;
+                    quads.Add(
+                        Instantiate(refObj, trans.TransformPoint(x + offset.x, y + offset.y, quadPoints.Z), Quaternion.LookRotation(-trans.forward,trans.up),transform));
+                }
+            }
+            DestroyImmediate(refObj,false);
+        }
+        
+        private void OnDrawGizmos()
+        {
+            var previous = quadPoints.LeftDown;
+            Gizmos.color = _colorSphere;
+            Gizmos.DrawWireSphere(transform.TransformPoint(previous),0.5f);
+            for (int i = 1; i < quadPoints.Points.Length; i++)
+            {
+                var current = quadPoints.Points[i];
+                Gizmos.color = _colorSphere;
+                Gizmos.DrawWireSphere(transform.TransformPoint(current),0.5f);
+                Gizmos.color = _colorLine;
+                Gizmos.DrawLine(transform.TransformPoint(previous),transform.TransformPoint(current));
+                previous = current;
+            }
+            Gizmos.DrawLine(transform.TransformPoint(quadPoints.LeftDown),transform.TransformPoint(previous));
+        }
+    }
+
+    [Serializable]
+    public struct Point
+    {
+
+        [SerializeField] private int z;
+        [SerializeField,MinValue(1)] private int multiplier;
+        [SerializeField] private Vector2Int leftDown;
+        [SerializeField] private Vector2Int rightUpper;
+
+        public Point(int z = 0)
+        {
+            this.z = z;
+            multiplier = 1;
+            leftDown = new Vector2Int(-1,-1);
+            rightUpper = new Vector2Int(1, 1);
+        }
+
+        public Point(int z, Vector2Int leftDown, Vector2Int rightUpper)
+        {
+            this.z = z;
+            this.multiplier = 1;
+            this.leftDown = leftDown;
+            this.rightUpper = rightUpper;
+        }
+
+        public int Z => z;
+        public Vector3Int LeftDown => new Vector3Int(leftDown.x*multiplier, leftDown.y*multiplier, z);
+        public Vector3Int RightDown => new Vector3Int(rightUpper.x*multiplier, leftDown.y*multiplier, z);
+        public Vector3Int RightUpper => new Vector3Int(rightUpper.x*multiplier, rightUpper.y*multiplier, z);
+        public Vector3Int LeftUpper => new Vector3Int(leftDown.x*multiplier, rightUpper.y*multiplier, z);
+
+        /// <summary>
+        /// Array with :<br />
+        ///  - LeftDown <br />
+        ///  - RightDown <br />
+        ///  - RightUpper <br />
+        ///  - LeftUpper <br />
+        /// </summary>
+        public Vector3Int[] Points => new[] {LeftDown, RightDown, RightUpper, LeftUpper};
+    }
+}
