d64faeb61dd7a200c110c0e2d921da3eb1e046cb
Iannis Poquet <ianpo@live.fr>
2021-01-31 20:20:35 +0100

    Update Screenshot360.cs

--- Assets/Resources/ProceduralCities/Screenshot360.cs
+++ Assets/Resources/ProceduralCities/Screenshot360.cs
@@ -10,16 +10,23 @@ namespace ProceduralCities
         [SerializeField] private string path;
         [SerializeField] private KeyCode screenshotKey = KeyCode.F10;
         [SerializeField, Required] private Camera camera;
-        
+        private string _keyScreen = "screenNumber";
+
+        private void Start()
+        {
+            if (!PlayerPrefs.HasKey(_keyScreen)) PlayerPrefs.SetInt(_keyScreen,0);
+        }
 
         private void Update()
         {
             if(Input.GetKeyDown(screenshotKey))
             {
                 //File.WriteAllBytes(path, I360Render.Capture(2048, true, camera, true));
+                PlayerPrefs.GetInt(_keyScreen);
                 var date = DateTime.Now;
                 ScreenCapture.CaptureScreenshot(
-                    "Screenshot - " + new Guid().ToString());
+                    "Screenshot - " + PlayerPrefs.GetInt(_keyScreen).ToString("000 000") + ".png");
+                PlayerPrefs.SetInt(_keyScreen, PlayerPrefs.GetInt(_keyScreen) + 1);
             }
         }
     }
